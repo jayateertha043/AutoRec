3bec4cfb26bd12efaf1e8bc36dcb38860c4f37ce
jayateertha043 <jayateertha043@gmail.com>
2020-05-14 21:22:33 +0530

    heroku slack

--- slack_config.py
+++ slack_config.py
@@ -1,4 +1,4 @@
-token='' #Enter Your Bot Token Here
-channel='' #Enter Your Channel Id here
+token='xoxb-1136520047089-1124648426515-Klzm5g48AmFQvEXovIHJUz2Y' #Enter Your Bot Token Here
+channel='C0140FA2A81' #Enter Your Channel Id here
 
 #Note:Your Bot must be added to channel before it can send the files or send message to that channel
