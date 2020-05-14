00002d2ab04017a7a5b10072582d498f0318065b
jayateertha043 <jayateertha043@gmail.com>
2020-05-14 20:47:25 +0530

    final

--- slack_config.py
+++ slack_config.py
@@ -1,2 +1,4 @@
-token='xoxb-1136520047089-1123928140722-JOUwbBzvHgLucuJUYJSlKQvs' #Enter Your Bot Token Here
-channel='C0140FA2A81' #Enter Your Channel Id here
+token='' #Enter Your Bot Token Here
+channel='' #Enter Your Channel Id here
+
+#Note:Your Bot must be added to channel before it can send the files or send message to that channel
