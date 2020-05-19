db4643bdebf33a9613b0cfb9fa65c636b838064a
jayateertha043 <jayateertha043@gmail.com>
2020-05-19 20:19:20 +0530

    hrokdhuuer

--- configs.py
+++
@@ -1,7 +0,0 @@
-token='xoxb-1136520047089-1124478999314-QVoOMud6xeqF86NQ7MZnUWZi' #Enter Your Bot Token Here
-channel='C0140FA2A81' #Enter Your Channel Id here
-
-#Note:Your Bot must be added to channel before it can send the files or send message to that channel
-
-#www2png.com
-APIKEY='7e19b41d-5ebc-48de-b7e1-7af436881c26'
