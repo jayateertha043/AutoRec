f6920d1e8343db1c3704df0eb6a2a5956eafe4a5
jayateertha043 <jayateertha043@gmail.com>
2020-05-19 14:25:23 +0530

    safe

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-token='xoxb-1136520047089-1124478999314-AD1UG3eGYbqndXFJjVjCUmWl' #Enter Your Bot Token Here
+token='xoxb-1136520047089-1124478999314-YRbiTdffbs1a3Xh9n2E7v7Jk' #Enter Your Bot Token Here
 channel='C0140FA2A81' #Enter Your Channel Id here
 
 #Note:Your Bot must be added to channel before it can send the files or send message to that channel
