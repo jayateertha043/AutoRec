81f9dc7badfc9e42d381bf6fd6c8f2ffcca9b3d7
jayateertha043 <jayateertha043@gmail.com>
2020-05-25 15:29:02 +0530

    environ

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
-token='' #Enter Your Bot Token Here
-channel='' #Enter Your Channel Id here
+import os
+
+if isHeroku in os.environ:
+    token=os.environ.get("token")
+    channel=os.environ.get("channel")
+    APIKEY=os.environ.get("APIKEY")
+else:
+    token='' #Enter Your Bot Token Here
+    channel='' #Enter Your Channel Id here
 
 #Note:Your Bot must be added to channel before it can send the files or send message to that channel
 
 #www2png.com api for screenshot free
-APIKEY=''
+    APIKEY=''
